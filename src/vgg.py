84257960403ba072a64cba26fa29bdde0cd0560d
Al Nejati <alireza.n.j@gmail.com>
2016-10-15 10:22:20 +1300

    Allow use of same model for training and inference By using train_mode instead of dropout_frac

--- src/vgg.py
+++ src/vgg.py
@@ -4,10 +4,10 @@ This model can then be saved as a GraphDef.
 
 This python file can be either included in another project,
 Or run as a standalone script (in which case it just constructs and saves the graph.)
-Example usage: vgg16 with input size 224 and no dropout, for inference:
-	python vgg.py 224 16 0.0 1000 models/vgg16.meta
-or, for instance, vgg19 with input size 64 with dropout, for training:
-	python vgg.py 64 19 0.5 1000 models/vgg19.meta
+Example usage: vgg16 with input size 224:
+	python vgg.py 224 16 1000 models/vgg16.meta
+or, for instance, vgg19 with input size 64:
+	python vgg.py 64 19 1000 models/vgg19.meta
 
 Author: Al Nejati
 TrainedEye Medical Technologies Ltd.
@@ -30,7 +30,7 @@ def conv_layer(x, in_channels, out_channels):
 		return tf.nn.relu(z)
 
 # A single fc (flat e.g. dense) layer
-def fc_layer(x, in_channels, out_channels, dropout_frac, name):
+def fc_layer(x, in_channels, out_channels, train_mode, dropout_frac, name):
 	with tf.variable_scope(name):
 		weights = tf.get_variable("weights",
 			                       shape=[in_channels, out_channels],
@@ -41,9 +41,7 @@ def fc_layer(x, in_channels, out_channels, dropout_frac, name):
 		y = tf.reshape(x, [-1, in_channels])
 		fc = tf.nn.bias_add(tf.matmul(y, weights), biases)
 		z = tf.nn.relu(fc)
-		if dropout_frac > 0.0:
-			z = tf.nn.dropout(z, dropout_frac)
-		return z 
+		return tf.cond(train_mode, lambda: tf.nn.dropout(z, dropout_frac), lambda: z)
 
 # A stack of n_convs convolutions followed by a max-pooling operation.
 # VGG has five of these blocks.
@@ -57,7 +55,7 @@ def conv_block(x, n_convs, n_input_channels, n_output_channels, name):
 		return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name="pool")
 
 # The VGG model. This is a stack of five conv_blocks plus three fc_layers.
-def vgg(x, num_classes, patch_size=224, vgg_type="16", dropout_frac=0.0):
+def vgg(x, train_mode, num_classes, patch_size=224, vgg_type="16"):
 	if vgg_type == "16":
 		nc = 3
 	elif vgg_type == "19":
@@ -71,17 +69,17 @@ def vgg(x, num_classes, patch_size=224, vgg_type="16", dropout_frac=0.0):
 		pool3 = conv_block(pool2, nc, 128, 256, "block_3")
 		pool4 = conv_block(pool3, nc, 256, 512, "block_4")
 		pool5 = conv_block(pool4, nc, 512, 512, "block_5")
-		fc6   = fc_layer(pool5, ((patch_size/(2**5))**2)*512, 4096, dropout_frac, "fc_6")
-		fc7   = fc_layer(fc6,   4096, 4096, dropout_frac, "fc_7")
-		fc8   = fc_layer(fc7,   4096, num_classes, 0.0, "fc_8")
+		fc6   = fc_layer(pool5, ((patch_size/(2**5))**2)*512, 4096, train_mode, 0.5, "fc_6")
+		fc7   = fc_layer(fc6,   4096, 4096, train_mode, 0.5, "fc_7")
+		fc8   = fc_layer(fc7,   4096, num_classes, tf.constant(False), 0.5, "fc_8")
 		return tf.nn.softmax(fc8, name="prob")
 
 if __name__ == "__main__":
 	patch_size   =   int(sys.argv[1])
 	vgg_type     =       sys.argv[2]
-	dropout_frac = float(sys.argv[3])
-	num_classes  =   int(sys.argv[4])
-	out_filename =       sys.argv[5]
+	num_classes  =   int(sys.argv[3])
+	out_filename =       sys.argv[4]
 	x = tf.placeholder(shape=[None, patch_size, patch_size, 3], dtype=tf.float32, name="input")
-	prob = vgg(x, num_classes, patch_size, vgg_type, dropout_frac)
+	train_mode = tf.placeholder(shape=[], dtype=tf.bool, name="train_mode")
+	prob = vgg(x, train_mode, num_classes, patch_size, vgg_type)
 	tf.train.export_meta_graph(filename=out_filename,as_text=True)
